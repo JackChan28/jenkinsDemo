8f105e03b618f71f9d51a962ddfd2c7791c87606
Jack <315379006@qq.com>
2019-12-09 16:41:51 +0800

    jenkins Demo

---
+++ src/main/java/com/jack/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.jack.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jack
+ * @site www.ctf.com.cn
+ * @company 周大福
+ * @create 2019-12-09 14:47
+ */
+@RestController
+public class DemoController {
+    @GetMapping(value = "/info")
+   public String Info()
+   {
+       return "Jekins Demo";
+   }
+}
